02823fbfb86cb3f7745de76394629892e934cd31
nguyenxuanquang30061999 <quang.nx0628@sinhvien.hoasen.edu.vn>
2021-01-16 13:13:56 +0700

    Project-Hackathon-w1

---
+++ Soucre code/script.js
@@ -0,0 +1,33 @@
+let database=[
+    {
+        name:"nestle-nan",
+        price: 910000,
+        img : "https://concung.com/dinh-duong-cho-be/nestle-nan-optipro-so-2-800g-6-12-thang-23775.html"
+
+    },
+    {
+        name:"combo-2",
+        price: 230000,
+        img: "https://concung.com/bot-an-dam/thuc-pham-bo-sung-gerber-organic-lua-mi-yen-mach-49270.html"
+    },
+    {
+        name:"ta-quan",
+        price:339000,
+        img: "https://concung.com/bim-ta-khuyen-mai/bim-ta-quan-huggies-dry-pants-super-jumbo-xl-12-17kg-62-mieng-20741.html"
+    },
+    {
+        name:"meji",
+        price:345000,
+        img:"https://concung.com/sua-bot/sua-thanh-meiji-ezcube-infant-formula-432g-0-12-thang-27424.html"
+    },
+    {
+        name:"huge",
+        price:339000,
+        img:"https://concung.com/bim-ta-khuyen-mai/bim-ta-quan-huggies-dry-pants-goi-cuc-dai-m-6-11kg-74-mieng-23787.html",
+    },
+    {
+        name:"glico",
+        price:495000,
+        img:"https://concung.com/sua-va-thuc-pham/glico-icreo-so-1-820g-1-3-tuoi-27393.html",
+    }
+];
